ff02f28b6e2625dc25ca7b6ea7e94cbdeb00f238
Brendan Nee <brendan.nee@gmail.com>
2012-03-31 17:33:55 -0400

    Test with SMS with known number

--- routes/index.js
+++ routes/index.js
@@ -27,13 +27,23 @@ module.exports = function routes(app){
 
   /* Routes */
   app.get('/api/incoming', function(req, res){
-    
     var toCall = hs[Math.floor(Math.random()*hs.length)];
 
     var twiml = '<?xml version="1.0" encoding="UTF-8" ?>\n';
-    
     twiml += '<Response>\n<Say>Welcome. Calling ' + toCall.name + '</Say>\n';
     twiml += '<Dial>' + toCall.number + '</Dial>\n';
+    twiml += '</Response>\n';
+    res.send(twiml, {'Content-Type':'text/xml'}, 200)
+  });
+
+  app.get('/api/sms', function(req, res){
+    var toText = hs[Math.floor(Math.random()*hs.length)];
+
+    var twiml = '<?xml version="1.0" encoding="UTF-8" ?>\n';
+    twiml += '<Response>\n';
+    twiml += '<Sms>Texting ' + toText.name + '</Sms>\n';
+    //twiml += '<Sms to="' + toText.number + '">' + req.params.Body + '</Sms>\n';
+    twiml += '<Sms to="4153736442">' + req.params.Body + '</Sms>\n';
     twiml += '</Response>\n';
     res.send(twiml, {'Content-Type':'text/xml'}, 200)
   });
